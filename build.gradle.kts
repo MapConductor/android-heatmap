3a2ca5ff873a48724dfae32a3d7db47fb92c62ec
Masashi Katsumata <wf9a5m75@gmail.com>
2026-07-01 21:57:46 +0900

    prepare for cmp

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,10 +57,10 @@ android {
 }
 
 dependencies {
-    if (findProject(":android-sdk-core") != null) {
-        implementation(project(":android-sdk-core"))
+    if (findProject(":android-sdk-compose") != null) {
+        implementation(project(":android-sdk-compose"))
     } else {
-        implementation("com.mapconductor:core:${project.findProperty("coreLibraryVersion") as String? ?: "1.0.0"}")
+        implementation("com.mapconductor:compose:${project.findProperty("coreLibraryVersion") as String? ?: "1.0.0"}")
     }
 
     // Coroutines for debounce and coordination
